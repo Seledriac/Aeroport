1c04b74f8c71680933f2da8c1eb47f487b25df68
Seledriac <tom.boumba7@gmail.com>
2020-12-28 17:13:00 +0100

    misc

---
+++ src/TODO.md
@@ -0,0 +1,7 @@
+* Headers - FAIT
+* Implémentations
+* Main de test des classes
+* CLI (Command Line Interface)
+- Partie admin (peut ajouter des vols, etc...)
+- Partie passager (peut réserver des vols, etc...)
+* Sauvegarde de l'état de l'app dans une bdd sqlite3.
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
-#include <Passager.hpp>
+// Commande de compilation qui marche : 
+// g++ ./main.cpp ./Headers/Administrateur.hpp ./Headers/Date.hpp ./Headers/Destination.hpp ./Headers/Passager.hpp ./Headers/Reservation.hpp ./Headers/Vol.hpp -o app 
 
 int main() {
-    Passager passager("Nom", "Prenom", "Homme", "5742895749", 65);
 }
